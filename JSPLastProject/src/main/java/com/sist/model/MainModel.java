52506685baf25e2c56534305e5180803a20ab6dd
SIST-101 <SIST-101@SIST>
2024-08-12 09:56:44 +0900

    1111

--- JSPLastProject/src/main/java/com/sist/model/MainModel.java
+++ JSPLastProject/src/main/java/com/sist/model/MainModel.java
@@ -4,12 +4,27 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.sist.controller.RequestMapping;
-
+import java.util.*;
+import com.sist.dao.*;
+import com.sist.vo.*;
 public class MainModel {
   @RequestMapping("main/main.do")
   public String main_main(HttpServletRequest request,HttpServletResponse response)
   {
+	    List<FoodVO> hitList=FoodDAO.foodHitTopData();
+	    List<FoodVO> likeList=FoodDAO.foodLikeTopData();
+	    List<FoodVO> jjimList=FoodDAO.foodJjimTopData();
+	    
+	    request.setAttribute("hitList", hitList);
+	    request.setAttribute("likeList", likeList);
+	    request.setAttribute("jjimList", jjimList);
+	    
 	    request.setAttribute("main_jsp", "../main/home.jsp");
 	    return "../main/main.jsp";
   }
 }
+
+
+
+
+
