d5deaecdff676e3ac0e0062cdadc98609558d5ab
SIST-101 <SIST-101@SIST>
2024-08-20 12:51:15 +0900

    1111

--- JSPLastProject/src/main/java/com/sist/dao/AllJjimDAO.java
+++ JSPLastProject/src/main/java/com/sist/dao/AllJjimDAO.java
@@ -85,4 +85,55 @@ public class AllJjimDAO {
 				  session.close();
 		  }
    }
+   /*
+    *   <select id="JjimMyPageListData" resultMap="jjimMap" parameterType="string">
+		   SELECT jno,cno,regdate,name,poster,address,phone
+		   FROM all_jjim aj,project_food_house pf
+		   WHERE aj.cno=pf.fno
+		   AND id=#{id}
+		   ORDER BY jno DESC
+		 </select>
+    */
+   public static List<AllJjimVO> JjimMyPageListData(String id)
+   {
+	   List<AllJjimVO> list=new ArrayList<AllJjimVO>();
+	   SqlSession session=null;
+		  try
+		  {
+			  session=ssf.openSession(true);
+			  list=session.selectList("JjimMyPageListData",id);
+		  }catch(Exception ex)
+		  {
+			  ex.printStackTrace();
+		  }
+		  finally
+		  {
+			  if(session!=null)
+				  session.close();
+		  }
+	   return list;
+   }
+   /*
+    *  <delete id="JjjimCancel" parameterType="int">
+		  DELETE FROM all_jjim
+		  WHERE jno=#{jno}
+		 </delete>
+    */
+   public static void JjjimCancel(int jno)
+   {
+	      SqlSession session=null;
+		  try
+		  {
+			  session=ssf.openSession(true);
+			  session.delete("JjjimCancel",jno);
+		  }catch(Exception ex)
+		  {
+			  ex.printStackTrace();
+		  }
+		  finally
+		  {
+			  if(session!=null)
+				  session.close();
+		  }
+   }
 }
